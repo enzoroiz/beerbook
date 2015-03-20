e2f7cb52a76487b01539ce2ab3a0b0d8db98109d
THippner <icelore@gmail.com>
2015-03-20 01:54:51 +0000

    Partial implementation of Search in Catalogue

--- beerbookapp/views_beercat.py
+++ beerbookapp/views_beercat.py
@@ -74,20 +74,88 @@ def beer_catalogue(request):
     context_dict = {}
 
     if request.method == 'POST':
-        # print request
-        if 'beername' in request.POST:
 
-            beer_name = request.POST['beername']
+        search_beer_type = request.POST['beertype']
+        search_beer_name = request.POST['beername']
+        search_beer_rating_max = request.POST['ratingmax']
+        search_beer_rating_min = request.POST['ratingmin']
 
-            try:
-                beer_dict = Beer.objects.get(name=beer_name)
-                context_dict['search_beer'] = beer_dict
-            except:
-                pass
+        # convert ' in strings to SQL escape ones
+        search_beer_name = search_beer_name.replace("'", "''")
 
+
+        # print "IN POST"
+        # print request.POST
+        # print ""
+
+        cursor = connection.cursor()
+        query_string = """
+                        select B.slug, B.name, T.name, ROUND(AVG(R.rating), 0)
+                        from beerbookapp_Beer B
+                        left outer join
+                        beerbookapp_Rating R
+                        on B.id = R.rated_beer_id
+                        left outer join
+                        beerbookapp_BeerType T
+                        on B.type_id = T.id """
+
+        if search_beer_name:
+            query_string += """where LOWER(B.name) LIKE '%""" + str(search_beer_name).lower() + "%' "
+
+        query_string += "group by B.id "
+
+        if int(search_beer_rating_min) == 0 and int(search_beer_rating_max) == 0:
+            pass
+        elif int(search_beer_rating_min) != 0 and int(search_beer_rating_max) != 0:
+            query_string += " having "
+            query_string += " ROUND(AVG(R.rating), 0) >= "
+            query_string += str(search_beer_rating_min)
+            query_string += " AND ROUND(AVG(R.rating), 0) <= "
+            query_string += str(search_beer_rating_max)
+        else:
+            if int(search_beer_rating_min) == 0:
+                query_string += " having "
+                query_string += " ROUND(AVG(R.rating), 0) <= "
+                query_string += str(search_beer_rating_max)
+                query_string += " OR ROUND(AVG(R.rating), 0) IS NULL"
+            elif int(search_beer_rating_max) == 0:
+                query_string += " having "
+                query_string += " ROUND(AVG(R.rating), 0) >= "
+                query_string += str(search_beer_rating_min)
+
+
+
+        # print "\n"
+        # print query_string
+
+        try:
+            cursor.execute(query_string)
+            query_result = cursor.fetchall()
+        finally:
+            cursor.close()
+
+        # print "\n"
+        # print query_result
+
+
+        beer_types = BeerType.objects.all()
+        context_dict['beer_types'] = beer_types
+        context_dict['beer_list'] = query_result
+
+        # # print request
+        # if 'beername' in request.POST:
+        #
+        #     beer_name = request.POST['beername']
+        #
+        #     try:
+        #         beer_dict = Beer.objects.get(name=beer_name)
+        #         context_dict['search_beer'] = beer_dict
+        #     except:
+        #         pass
     else:
         beer_types = BeerType.objects.all()
 
+        print "IN ELSE"
         cursor = connection.cursor()
         cursor.execute("""
                         select B.slug, B.name, T.name, ROUND(AVG(R.rating), 0)
