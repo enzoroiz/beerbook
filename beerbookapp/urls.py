2442556c06a6b0f3e40d5c11b4b955d296aeddf2
THippner <icelore@gmail.com>
2015-03-21 17:48:28 +0000

    Events can be viewed now

--- beerbookapp/urls.py
+++ beerbookapp/urls.py
@@ -5,6 +5,7 @@ from beerbookapp import views, views_beercat, views_events
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^event_catalogue/(?P<event_id>[\w\-]+)/$', views_events.event, name='event'),
                        url(r'^event_catalogue/$', views_events.event_catalogue, name='event_catalogue'),
                        url(r'^add_rating/$', views_beercat.add_rating, name='add_rating'),
                        url(r'^beer_catalogue/(?P<beer_name_slug>[\w\-]+)/$', views_beercat.beer, name='beer'),
--- beerbookapp/views_events.py
+++ beerbookapp/views_events.py
@@ -7,13 +7,31 @@ from django.contrib.auth.models import User
 from django.db import connection
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from beerbookapp.models import Event
+
+
+def event(request, event_id):
+    context_dict = {}
+
+
+    # print event_id
+
+    try:
+        this_event = Event.objects.get(id=event_id)
+        # print this_event
+        context_dict['event'] = this_event
+    except:
+        pass
+
+    response = render(request, 'beerbookapp/event.html', context_dict)
+    return response
 
 
 def event_catalogue(request):
 
     context_dict = {}
 
-    sql_query = "select E.title, E.datetime, U.username " \
+    sql_query = "select E.title, E.datetime, U.username, E.id " \
                 "from beerbookapp_Event E " \
                 "join auth_user U " \
                 "on E.owner_id = U.id " \
